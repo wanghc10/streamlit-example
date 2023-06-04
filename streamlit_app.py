3b32cdb6e155dc7a5698f30118463c278accff50
wanghc10 <wanghc10@163.com>
2023-06-04 09:38:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,5 +34,5 @@ with st.echo(code_location='below'):
         data.append(Point(x, y))
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
+        .mark_circle(color='#0088c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
